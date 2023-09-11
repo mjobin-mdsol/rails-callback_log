aa0d68a632e9091180632173ca04c498cf69abd4
Mathieu Jobin <majobin@mdsol.com>
2023-09-11 14:22:47 +0900

    Update rails-callback_log.rb

--- lib/rails-callback_log.rb
+++ lib/rails-callback_log.rb
@@ -38,8 +38,8 @@ module RailsCallbackLog
     # @override_target will be set. If the callback was a symbol method name
     # then @method_name will be set.
     def expand(target, value, block)
-      ::RailsCallbackLog.log(target.inspect)
-      ::RailsCallbackLog.log(@override_block || @override_target || @method_name)
+      event = "#{target.class.to_s} :: #{@override_block || @override_target || @method_name}"
+      ::RailsCallbackLog.log(event)
 
       super(target, value, block)
     end
